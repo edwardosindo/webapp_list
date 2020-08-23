a9f1cdf1274e0071227e69737a73680ff0ec5912
Edward Osindo <eddu.almasi@gmail.com>
2020-08-23 20:09:53 +0300

    database config

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn codedaddies.wsgi --log-file -
+
+
--- codedaddies/settings.py
+++ codedaddies/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'codedaddies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'test',
+        'USER': 'test',
+        'PASSWORD':'test.',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
